06ffd899cf7802d3bf46607f3c25760f64a13fea
Ngnawen Samuel <ngnawens@gmail.com>
2022-05-06 21:08:32 +0400

    trackBy implement

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.3.
 
+## Description
+
+This project present how to use `trackBy` function. ``trackBy ` is use to improve the performance of an angular application. 
+With large collection of data, angular `ngFor` structure directive may perform poorly. When we use `trackBy` function, all data in the DOM will not recreated.
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'TrackByInAngular';
+  store: any;
+  isTrackByMode: boolean = true;
+  isNotTrackByMode: boolean = true;
+
+  constructor() {
+    this.getData();
+  }
+
+  getData() {
+    this.store = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Doe' },
+      { id: 3, name: 'Test' },
+    ];
+  }
+
+  trackByFn(index:number, item:any) {
+    return index;
+  }
+
+  getNewData() {
+    this.store = [
+      { id: 1, name: 'John' },
+      { id: 2, name: 'Doe' },
+      { id: 3, name: 'Test' },
+      { id: 4, name: 'Sam' },
+      { id: 5, name: 'Kelly' },
+      { id: 6, name: 'Thor' },
+    ];
+  }
 }
